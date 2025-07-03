e3272e14e1a8270ac54514db08d953fad76b89a0
Andres <andresito15071999@gmail.com>
2025-07-03 02:52:31 -0500

    --

--- tests/test_lambda_reclamos.py
+++ tests/test_lambda_reclamos.py
@@ -9,14 +9,21 @@ from moto import mock_aws
 # 1. Agregamos el directorio 'src' al path para que Python encuentre los módulos
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-# 2. Creamos un contexto de AWS simulado y definimos las variables ANTES de importar la app
-# Usamos un decorador en una función vacía para "activar" moto y poder crear la cola
+# 2. Creamos un contexto de AWS simulado y definimos TODAS las variables ANTES de importar la app
 @mock_aws
 def setup_mock_environment():
-    sqs = boto3.client('sqs', region_name='us-east-2')
-    response = sqs.create_queue(QueueName='cola-test')
-    os.environ['SQS_QUEUE_URL'] = response['QueueUrl']
-    os.environ['DYNAMODB_TABLE_RECLAMOS'] = 'tabla-test-reclamos'
+    # Definimos el nombre de la tabla y la cola
+    table_name = 'tabla-test-reclamos'
+    queue_name = 'cola-test'
+    aws_region = 'us-east-2'
+    
+    # Creamos la cola SQS simulada y obtenemos su URL
+    sqs = boto3.client('sqs', region_name=aws_region)
+    sqs_response = sqs.create_queue(QueueName=queue_name)
+    
+    # Ponemos las variables de entorno que la aplicación necesita para importarse
+    os.environ['SQS_QUEUE_URL'] = sqs_response['QueueUrl']
+    os.environ['DYNAMODB_TABLE_RECLAMOS'] = table_name
 
 # Ejecutamos la función para que las variables de entorno existan
 setup_mock_environment()
@@ -30,10 +37,13 @@ class TestLambdaReclamos(unittest.TestCase):
 
     def setUp(self):
         """
-        Este método ahora solo se encarga de crear la tabla de DynamoDB
-        antes de cada prueba. La cola SQS ya fue creada.
+        Este método ahora solo se encarga de crear los recursos de AWS simulados
+        usando los nombres de las variables de entorno que ya existen.
         """
-        dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+        aws_region = 'us-east-2'
+        
+        # Setup DynamoDB
+        dynamodb = boto3.resource('dynamodb', region_name=aws_region)
         table_name = os.environ['DYNAMODB_TABLE_RECLAMOS']
         dynamodb.create_table(
             TableName=table_name,
